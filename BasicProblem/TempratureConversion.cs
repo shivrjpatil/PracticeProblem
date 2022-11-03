d2966110446d5e13a0558a75e13570a2ad4ae572
shivrjpatil <shivrajp972@gmail.com>
2022-11-03 21:26:07 +0530

    [shiv]add:temprature converter

---
+++ BasicProblem/TempratureConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class TempratureConversion
+    {
+        public void Celsius(double deg)
+        {
+            double Farenhit = (deg * (9 / 5) + 32);
+                Console.WriteLine(Farenhit);
+        }
+        public void Farenhit(double deg)
+        {
+            double Celsius = (deg - 32) * 5 / 9;
+            Console.WriteLine(Celsius);
+        }
+
+    }
+}
