931a2800a515055ad84916d474f7e1494143b804
shivrjpatil <shivrajp972@gmail.com>
2022-11-04 23:48:16 +0530

    [shiv]add:montly payment

---
+++ BasicProblem/Monthlypayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Monthlypayment
+    {
+        public void pay()
+        {
+            Console.WriteLine("enter the principle amount:");
+            double P=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the year:");
+            double Y=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the intrest:");
+            double R=Convert.ToInt32(Console.ReadLine());
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double a = (1 + r);
+            double b = (-n);
+
+            double payment = (P * r) / 1 - Math.Pow(a,b);
+            Console.WriteLine(payment);
+
+
+        }
+    }
+}
