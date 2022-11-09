aeeba4b657357db8e9dcf6c1c84daf5b1c2806df
shivrjpatil <shivrajp972@gmail.com>
2022-11-09 17:48:56 +0530

    [shiv]add:check vowel or consonant

---
+++ BasicProblem/Vowel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Vowel
+    {
+        public void Conso()
+        {
+            char alpha;
+            Console.WriteLine("enter the alphabet:");
+            alpha = Convert.ToChar(Console.ReadLine().ToLower());
+            switch(alpha)
+            {
+                case 'a':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("vowel");
+                    break ;
+                case 'e':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("vowel");
+                    break;
+                default:
+                    Console.WriteLine("consonant");
+                    break;
+            }
+        }
+    }
+}
