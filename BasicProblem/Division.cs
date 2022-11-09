75a414d41877e4eeaaf057eb1865c06438ffd201
shivrjpatil <shivrajp972@gmail.com>
2022-11-09 17:31:30 +0530

    [shiv]add:division

---
+++ BasicProblem/Division.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Division
+    {
+        public void Div(int a,int b)
+        {
+            int Quotient = a / b;
+            int Rem = a % b;
+            Console.WriteLine("the quoetient and remiander is :" + Quotient,+ Rem);
+            Console.WriteLine( Rem);    
+        }
+    }
+} 
