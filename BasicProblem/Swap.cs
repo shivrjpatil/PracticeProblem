a173a4b75dcdecb873a4ae80b70954850ac6d537
shivrjpatil <shivrajp972@gmail.com>
2022-11-09 17:43:41 +0530

    [shiv]add:swap two numbers

---
+++ BasicProblem/Swap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Swap
+    {
+        public void Number(int a ,int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
