8ed26df8b9146ec96d7370cc62709bd5eb6d68bd
shivrjpatil <shivrajp972@gmail.com>
2022-11-09 17:46:20 +0530

    [shiv]add:check even odd numbers

---
+++ BasicProblem/EvenOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class EvenOdd
+    {
+        public void check(double a)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else
+                Console.WriteLine("the number is odd");
+        }
+    }
+}
